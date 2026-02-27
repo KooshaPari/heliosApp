f12775ef08e374c14c0c6c5684b1978560970da0
Koosha Paridehpour <kooshapari@gmail.com>
2026-02-27 05:03:10 -0700

    feat(WP01): renderer adapter interface

--- apps/runtime/src/renderer/index.ts
+++ apps/runtime/src/renderer/index.ts
@@ -20,7 +20,7 @@ export type {
 export type {
   RendererCapabilities,
   CapabilityDiff,
-  CapabilityDiff as CapabilityDiffEntry,
+  CapabilityDiffEntry,
 } from "./capabilities.js";
 export { queryCapabilities, compareCapabilities } from "./capabilities.js";
 
@@ -47,56 +47,6 @@ export {
   SwitchTimeoutError,
   SwitchSameRendererError,
 } from "./switch.js";
-
-export type {
-  StreamBinding,
-  BufferOverflowEvent,
-  StreamBindingEventBus,
-} from "./stream_binding.js";
-export {
-  StreamBindingManager,
-  SwitchBuffer,
-} from "./stream_binding.js";
-
-export type {
-  HotSwapResult,
-  TerminalContext,
-} from "./hot_swap.js";
-export {
-  executeHotSwap,
-  HotSwapError,
-  HotSwapCapabilityError,
-} from "./hot_swap.js";
-
-export type {
-  RollbackResult,
-  RollbackTerminalStatus,
-} from "./rollback.js";
-export {
-  executeRollback,
-  RollbackError,
-} from "./rollback.js";
-
-export type {
-  SwitchTransaction,
-  SwitchTransactionRequest,
-  SwitchTransactionState,
-} from "./switch_transaction.js";
-export {
-  SwitchTransactionOrchestrator,
-  createSwitchOrchestrator,
-  ConcurrentSwitchError,
-  InvalidTransitionError,
-} from "./switch_transaction.js";
-
-export type {
-  ZmxCheckpoint,
-  RestartRestoreResult,
-} from "./restart_restore.js";
-export {
-  executeRestartWithRestore,
-  RestartRestoreError,
-} from "./restart_restore.js";
 
 // ---------------------------------------------------------------------------
 // Lifecycle event types
